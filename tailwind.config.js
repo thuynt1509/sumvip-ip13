37326254f5b01ec3cc75b5c43e26e40e4b3ada24
thuynt <thuthuy.nguyen1509@gmail.com>
2023-02-11 23:39:02 +0700

    update all

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,39 +5,40 @@ module.exports = {
         "./node_modules/tw-elements/dist/js/**/*.js"
     ],
     theme: {
-        fontFamily: {
-            'MyriadPro': ['Myriad Pro Regular', 'sans-serif'],
-            'MyriadProBold': ['Myriad Pro Bold', 'sans-serif'],
-            'MyriadProSemiBold': ['Myriad Pro SemiBold', 'sans-serif'],
-        },
-        screens: {
-            // 'xxl': {max: '1431px'},
-            'xl': { max: '1119px' },
-            'lg': { max: '1023px' },
-            'md': { max: '767px' },
-            'sm': { max: '639px' }
-        },
-        container: {
-            padding: {
-                xl: "0 1rem",
-            }
-        },
-        backgroundPosition: {
-            center: 'center top',
+      fontFamily: {
+        'MyriadPro': ['Myriad Pro Regular', 'sans-serif'],
+        'MyriadProBold': ['Myriad Pro Bold', 'sans-serif'], 
+        'MyriadProSemiBold': ['Myriad Pro SemiBold', 'sans-serif'], 
+      },
+      screens:{
+        // 'xxl': {max: '1431px'},
+        'xl': {max: '1119px'},
+        'lg': {max: '1023px'},
+        'md': {max: '767px'},
+        'sm': {max: '639px'}
+      },
+      container:{
+        padding: {
+          xl: "0 1rem",
         }
+      },
+      backgroundPosition: {
+        center: 'center top',
+      }
     },
     plugins: [
-        require('tw-elements/dist/plugin'),
-        function({ addComponents }) {
-            addComponents({
-                '.container': {
-                    maxWidth: '1200px',
-                    '@screen xl': {
-                        maxWidth: '1119px',
-                        padding: '0 10px'
-                    },
-                }
-            })
-        }
+      require('tw-elements/dist/plugin'),
+      function ({ addComponents }) {
+        addComponents({
+          '.container': {
+            maxWidth: '1200px',
+            '@screen xl': {
+              maxWidth: '1119px',
+              padding: '0 10px'
+            },
+          }
+        })
+      }
     ]
-}
+  }
+  
